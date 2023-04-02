c744271660114e54a766ab938317b7da7cc1c1e0
bilalabdelkadir <bilalbinabdelkadir>
2023-04-02 15:16:01 +0300

    finished step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 
-const Statics = ({ good, bad, neutral }) => {
+const Statics = ({ good, bad, neutral, sum }) => {
+  if (sum === 0) {
+    return <p>No Feedback given</p>;
+  }
   return (
     <div>
-      <h1>Statics</h1>
       <p>Good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>avarage {(good * 1 + bad * -1) / (good + bad + neutral)}</p>
-      <p>posotive {(good * 1) / (good + bad + neutral)}%</p>
+      <p>all {sum}</p>
+      <p>avarage {(good * 1 + bad * -1) / sum}</p>
+      <p>posotive {(good * 1) / sum}%</p>
     </div>
   );
 };
@@ -19,16 +21,33 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+    setSum(sum + 1);
+  };
+
+  const handleBad = () => {
+    setBad(good + 1);
+    setSum(sum + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(good + 1);
+    setSum(sum + 1);
+  };
 
   return (
     <div>
       <h1>Give Feed Back</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>Good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <button onClick={handleGood}>Good</button>
+        <button onClick={handleBad}>neutral</button>
+        <button onClick={handleNeutral}>bad</button>
       </div>
-      <Statics good={good} bad={bad} neutral={neutral} />
+      <h1>Statics</h1>
+      <Statics good={good} bad={bad} neutral={neutral} sum={sum} />
     </div>
   );
 };
