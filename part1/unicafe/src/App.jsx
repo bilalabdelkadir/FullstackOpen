a15ef05a510130166bc2fb9b2d01679d370a01c8
bilalabdelkadir <bilalbinabdelkadir>
2023-04-02 15:08:29 +0300

    finished step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+const Statics = ({ good, bad, neutral }) => {
+  return (
+    <div>
+      <h1>Statics</h1>
+      <p>Good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + bad + neutral}</p>
+      <p>avarage {(good * 1 + bad * -1) / (good + bad + neutral)}</p>
+      <p>posotive {(good * 1) / (good + bad + neutral)}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -14,15 +28,7 @@ const App = () => {
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
-      <div>
-        <h1>Statics</h1>
-        <p>Good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + bad + neutral}</p>
-        <p>avarage {(good * 1 + bad * -1) / (good + bad + neutral)}</p>
-        <p>posotive {(good * 1) / (good + bad + neutral)}%</p>
-      </div>
+      <Statics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
