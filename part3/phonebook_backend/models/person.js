d2c9c5ec37d4f4c309f4e1eca09b493ec56dadd8
bilalabdelkadir <bilalbinabdelkadir>
2023-04-15 15:01:06 +0300

    added number validation

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -15,12 +15,36 @@ mongoose
     console.log("error connecting to MongoDB:", error.message);
   });
 
+const numberValidators = [
+  {
+    // Minimum length validator
+    validator: (number) => {
+      if ((number[2] === "-" || number[3] === "-") && number.length < 9) {
+        return false;
+      }
+      return true;
+    },
+    msg: "must be at least 8 digits",
+  },
+  {
+    // Regex validator to allow only numbers
+    validator: (number) => {
+      return /^\d{2,3}-\d+$/.test(number);
+    },
+    msg: "invalid phone number",
+  },
+];
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: numberValidators,
+    required: true,
+  },
 });
 
 personSchema.set("toJSON", {
