a75afe481435e9511e6033567397976708578def
bilalabdelkadir <bilalbinabdelkadir@gmail.com>
2023-07-02 16:51:11 +0300

    added favourite blog function and test

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,32 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => {
+    return total + blog.likes;
+  }, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  let maxLikes = 0;
+  let favorite = null;
+  blogs.forEach((blog) => {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes;
+      favorite = blog;
+    }
+  });
+
+  if (favorite) {
+    const { title, author, likes } = favorite;
+    return { title, author, likes };
+  } else {
+    return null;
+  }
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
