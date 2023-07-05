711001a274135f75059014077f00f702378c471c
bilalabdelkadir <bilalbinabdelkadir@gmail.com>
2023-07-05 16:24:22 +0300

    added default value of like to be 0

--- part4/test/blog_api.test.js
+++ part4/test/blog_api.test.js
@@ -56,6 +56,21 @@ test("a valid blog can be added", async () => {
   expect(titles).toContain("this is custom one");
 });
 
+test("a blog with no like default value should be 0", async () => {
+  const blogObject = {
+    title: "this is custom one",
+    author: "bilal",
+    url: "randomone.com",
+  };
+  const response = await api
+    .post("/api/blogs")
+    .send(blogObject)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  expect(response.body.likes).toBe(0);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
