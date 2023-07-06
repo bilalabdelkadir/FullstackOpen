560f7a39b4fdbcc8b86b7d9a4ccef5844956c22b
bilalabdelkadir <bilalbinabdelkadir@gmail.com>
2023-07-06 11:30:14 +0300

    bloglist expansion, step4

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -5,6 +5,12 @@ const bcrypt = require("bcrypt");
 userRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (username.length < 3 || name.length < 3) {
+    return response
+      .status(400)
+      .json({ message: "username and user must be longer than 3 char" });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
