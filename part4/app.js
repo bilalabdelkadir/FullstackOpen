1c4bfc31d505b3cd89f63cd0fff9cbada7e9d47c
bilalabdelkadir <bilalbinabdelkadir@gmail.com>
2023-07-06 12:20:23 +0300

    added jwt login functionality

--- part4/app.js
+++ part4/app.js
@@ -8,6 +8,7 @@ const blogsRouter = require("./controllers/blogs");
 const userRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger.js");
+const loginRouter = require("./controllers/login");
 const app = express();
 
 mongoose.set("strictQuery", false);
@@ -30,6 +31,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
